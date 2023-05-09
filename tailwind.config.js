c5788b223f0b4942c7acd1f0ef66f158e91e9c67
issa99-lab <issanana99@gmail.com>
2023-05-09 17:23:59 +0300

    Nav done :)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,33 @@ module.exports = {
   content: ['./*.html'],
   theme: {
     extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '976px',
+    },
+    colors: {
+      'orange': 'hsl(26, 100%, 55%)',
+      'pale- orange': 'hsl(25, 100%, 94%)',
+      'very-dark-blue': 'hsl(220, 13%, 13%)',
+      'dark-grayish-blue': 'hsl(219, 9%, 45%)',
+      'grayish-blue': 'hsl(220, 14%, 75%)',
+      'light-grayish-blue': 'hsl(223, 64%, 98%)',
+      'black': 'hsl(0, 0%, 0%)',
+      'white': 'hsl(0, 0%, 100%)'
+    },
+    fontFamily: {
+      'sans-serif': ['Kumbh Sans'],
+    },
+    fontWeight: {
+      regular: '400',
+      bold: '700'
+    },
+    fontSize: {
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '2rem',
+    },
   },
   plugins: [],
 }
