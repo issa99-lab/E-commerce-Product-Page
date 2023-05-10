481244409bff37f9f183d4f2010cfb3b058ac40e
issa99-lab <issanana99@gmail.com>
2023-05-11 00:14:53 +0300

    Add and Minus function on click done

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const plus = document.querySelector('.fa-plus');
+const minus = document.querySelector('.fa-minus');
+const items = document.getElementById('numItem');
+const addToCart = document.getElementById('addToCart');
+
+
+let count = 0;
+plus.addEventListener('click', function () {
+  if (count < 50) {
+    count++;
+    items.innerText = count;
+  }
+});
+
+
+minus.addEventListener('click', function () {
+  if (count > 0) {
+    count--;
+    items.innerText = count;
+  }
+});
